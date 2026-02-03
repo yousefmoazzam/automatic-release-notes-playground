73b7009fdead290f6189e83efc3845d1752d4aa3
Yousef Moazzam <yousef.moazzam@diamond.ac.uk>
2026-02-03 17:13:33 +0000

    Add config to extract and upload user announcement changes as release asset

---
+++ .github/extract-user-announcement-changes.py
@@ -0,0 +1,66 @@
+from typing import Optional
+
+import requests
+import sys
+
+FILENAME = "user-announcements.txt"
+RELEASE_URL = "https://api.github.com/repos/yousefmoazzam/automatic-release-notes-playground/releases/tags/"
+USER_ANNOUNCEMENT_HEADER = "Notable Changes for Users"
+
+
+def main(upload_url: str, auth_token: str, tag: str):
+    release_body = get_release_body(tag)
+    data = extract_user_announcements(release_body)
+
+    if data is not None:
+        upload_asset(FILENAME, data, upload_url, auth_token)
+
+
+def get_release_body(tag: str) -> str:
+    url = RELEASE_URL + tag
+    resp = requests.get(url=url)
+    return resp.json()["body"]
+
+
+def extract_user_announcements(text: str) -> Optional[str]:
+    if USER_ANNOUNCEMENT_HEADER not in text:
+        return None
+
+    lines = text.splitlines()
+    idx = lines.index(f"### {USER_ANNOUNCEMENT_HEADER}")
+
+    notes = []
+    for line in lines[idx + 1 :]:
+        if line.startswith("*"):
+            pr_title = line.split(" by")[0]
+            notes.append(pr_title)
+            continue
+        break
+
+    return str.join("\n", notes)
+
+
+def upload_asset(filename: str, data: str, url: str, token: str):
+    url_split = url.split("{?name,label}")
+    url = url_split[0]
+    query_params = {"name": filename}
+    headers = {
+        "Authorization": f"Bearer {token}",
+        "Content-Type": "application/octet-stream",
+    }
+    resp = requests.post(
+        url=url, params=query_params, data=data.encode(), headers=headers
+    )
+    print(resp.json())
+
+
+if __name__ == "__main__":
+    args = sys.argv[1:]
+
+    if len(args) != 3:
+        print(
+            "Usage: python extract-user-announcement-changes.py <upload-url> <auth-token> <tag>"
+        )
+        sys.exit(1)
+
+    main(args[0], args[1], args[2])
